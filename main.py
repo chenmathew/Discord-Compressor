d0689528b11762fa8691e6cb72c60179da3a14a7
chenmathew <62050214+chenmathew@users.noreply.github.com>
2021-05-17 03:46:42 -0700

    Update main.py

--- main.py
+++ main.py
@@ -2,10 +2,6 @@ from __future__ import unicode_literals
 import discord
 from discord import File
 import asyncio
-from flask import Flask
-from threading import Thread
-import itertools
-from itertools import cycle
 from discord.ext import commands, tasks
 import youtube_dl
 import os
@@ -16,8 +12,6 @@ import ffmpeg
 
 my_secret = os.environ['my secret']
 
-app = Flask('')
-status = itertools.cycle(['task1','task2'])
 def compress_video():
     min_audio_bitrate = 32000
     max_audio_bitrate = 256000
@@ -41,51 +35,31 @@ def compress_video():
     video_bitrate = target_total_bitrate - audio_bitrate
     video_bitrate = str(video_bitrate)
     subprocess.call(["ffmpeg", "-i", "video.mp4", "-vcodec", "h264", "-b:v", video_bitrate, "-acodec", "mp3" ,"-preset", "ultrafast", "output.mp4"])
- 
-
-
-@app.route('/')
-def main():
-  return "Your Bot Is Ready"
-
-def run():
-  app.run(host="0.0.0.0", port=8000)
-
-def keep_alive():
-  server = Thread(target=run)
-  server.start()
-
-
 
 client = discord.Client()
 
 @client.event
 async def on_ready():
-  change_status.start()
   print('BOT IS ONLINE')
-
-@tasks.loop(seconds=10)
-async def change_status():
-  await client.change_presence(activity=discord.Game(next(status)))
 
 @client.event
 async def on_message(message):
   if message.author == client.user:
     return
-  if message.content.startswith('https://vm.tiktok.com/'):
+  if (message.content.startswith('https://vm.tiktok.com/') or message.content.startswith('https://twitter.com/')):
     ydl_opts = {'outtmpl': 'video.mp4'}
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-      ydl.download([message.content])
-  if message.content.startswith('https://twitter.com/'):
-    ydl_opts = {'outtmpl': 'video.mp4'}
-    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-      ydl.download([message.content])
-    await message.channel.send(file=File("video.mp4")) 
-  if ((message.author.id == 836198988629278720) and (message.content.startswith('File too big.'))):
-    compress_video()
-    await message.channel.send(file=File("output.mp4"))
-    os.remove('video.mp4')
-    os.remove('output.mp4')
+        ydl.download([message.content])
+    try:
+        await message.channel.send(file=File("video.mp4"))
+        pass
+    except discord.HTTPException:
+        compress_video()
+        await message.channel.send(file=File("output.mp4"))
+        pass
+    if (os.path.isfile('video.mp4')):
+        os.remove('video.mp4')
+    if (os.path.isfile('output.mp4')):
+        os.remove('output.mp4')
    
-keep_alive()
 client.run(my_secret)
